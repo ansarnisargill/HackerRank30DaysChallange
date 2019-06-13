9a2c29f27a29609af861bb59a4c518265d5e6942
Ansar <ansarnisargill@gmail.com>
2019-06-13 09:13:50 +0500

    Day 2 challange

---
+++ HackerRankPractice/Classes/Day2.cs
@@ -0,0 +1,45 @@
+﻿
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+namespace HackerRankPractice
+{
+    public class SolutionDay2
+    {
+
+        // Complete the solve function below.
+        public void solve(double meal_cost, int tip_percent, int tax_percent)
+        {
+            var tip = meal_cost * (tip_percent / 100.0);
+            var tax = meal_cost * (tax_percent / 100.0);
+            var total_cost = Math.Round(meal_cost + tip + tax,0);
+            Console.WriteLine(total_cost);
+            Console.Read();
+
+        }
+
+        
+        public SolutionDay2()
+        {
+            Console.WriteLine("Enter Meal Cost");
+            double meal_cost = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter tip percentage");
+            int tip_percent = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter tax percentage");
+            int tax_percent = Convert.ToInt32(Console.ReadLine());
+            solve(meal_cost, tip_percent, tax_percent);
+        }
+    }
+}
+
+
---
+++ HackerRankPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HackerRankPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SolutionDay2 obj = new SolutionDay2();
+        }
+    }
+}
